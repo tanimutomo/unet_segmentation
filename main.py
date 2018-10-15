5164bd1fa24479b2383b5c06743649a5c7654d32
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-10-15 13:06:48 +0900

    revise (violently)

--- main.py
+++ main.py
@@ -22,46 +22,48 @@ from src.model import UNet
 from src.utils import *
 
 
-parser = argparse.ArgumentParser(description='Params')
-parser.add_argument('--cml', action='store_true')
-parser.add_argument('--epochs', type=int, default=1000)
-parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--save_name', type=int, default=100)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Params')
+# parser.add_argument('--cml', action='store_true')
+# parser.add_argument('--epochs', type=int, default=1000)
+# parser.add_argument('--lr', type=float, default=1e-3)
+# parser.add_argument('--save_name', type=int, default=100)
+# args = parser.parse_args()
 
-if args.cml:
+
+params = {
+        'epochs': 1000,
+        'bs': 4,
+        'lr': 1e-3,
+        'momentum': 0.9,
+        'init_size': (256, 256),
+        'bn': True,
+        'visualize': True,
+        'save_name': 100,
+        'num_classes': 21,
+        'cml': True
+        }
+
+if params['cml']:
     experiment = Experiment(api_key="xK18bJy5xiPuPf9Dptr43ZuMk",
             project_name="U-Net_VOC", workspace="tanimutomo")
 else:
     experiment = None
 
-hyper_params = {
-        'epochs': args.epochs,
-        'bs': 4,
-        'lr': args.lr,
-        'momentum': 0.9,
-        'init_size': (256, 256),
-        'bn': True,
-        'visualize': True,
-        'save_name': args.save_name,
-        'num_classes': 21,
-        }
-
 if args.cml:
-    experiment.log_multiple_params(hyper_params)
+    experiment.log_multiple_params(params)
 
 data_root = './data/VOC'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = UNet(hyper_params['bn'])
+model = UNet(params['bn'])
 criterion = nn.CrossEntropyLoss(ignore_index=255)
-optim = optim.SGD(model.parameters(), lr=hyper_params['lr'], momentum=hyper_params['momentum'])
-train_loader, val_loader = get_loader(data_root, hyper_params['bs'], hyper_params['init_size'])
+optim = optim.SGD(model.parameters(), lr=params['lr'], momentum=params['momentum'])
+train_loader, val_loader = get_loader(data_root, params['bs'], params['init_size'])
 
 if os.path.exists('./model') == 0:
     os.mkdir('./model')
 if os.path.exists('./image') == 0:
     os.mkdir('./image')
 
-trainer = Trainer(device, model, criterion, optim, train_loader, val_loader, hyper_params, experiment)
+trainer = Trainer(device, model, criterion, optim, train_loader, val_loader, params, experiment)
 trainer.iteration()
 
